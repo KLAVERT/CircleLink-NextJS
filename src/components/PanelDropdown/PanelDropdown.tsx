e88e6ab781d613f83c3325b7afb0e005dfd9799a
GamenHaaDee <75490815+GamenHaaDee@users.noreply.github.com>
2025-02-27 20:32:24 +0100

    Update PanelDropdown.tsx

--- src/components/PanelDropdown/PanelDropdown.tsx
+++ src/components/PanelDropdown/PanelDropdown.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import Dropdown from '../Dropdown/Dropdown';
 
